aa658726192a2e886101e254d34e120f2a08d10f
Kamil Kałuża <kamil321.94@gmail.com>
2022-03-21 19:31:39 +0100

    postempy 21-03

--- WizualizacjaDanych/scatter_squares.py
+++ WizualizacjaDanych/scatter_squares.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
-plt.plot(input_values, squares, linewidth=5)
+
+plt.scatter(x_values, y_values, s=40)
 # Zdefiniowanie tytułu wykresu i etykiety osi
 plt.title("Kwadraty liczb", size=24)
 plt.xlabel("Wartość", size=14)
 plt.ylabel("Kwadraty wartości", size=14)
 
 # Zdefiniowanie wielkości etykiet
-plt.tick_params(axis='both', labelsize=14)
-
+plt.tick_params(axis="both", which="major", labelsize=14)
+# Zdefiniowanie zakresu dla każdej osi.
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
