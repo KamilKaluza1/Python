be3311385abb8b9142f63d84b23719894c90a16e
Kamil Kałuża <kamil321.94@gmail.com>
2022-03-24 17:28:33 +0100

    podstępy 22-03

--- WizualizacjaDanych/scatter_squares.py
+++ WizualizacjaDanych/scatter_squares.py
@@ -3,7 +3,7 @@ x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, s=30, edgecolors='none', c=y_values, cmap=plt.cm.Greens)
 # Zdefiniowanie tytułu wykresu i etykiety osi
 plt.title("Kwadraty liczb", size=24)
 plt.xlabel("Wartość", size=14)
@@ -12,5 +12,5 @@ plt.ylabel("Kwadraty wartości", size=14)
 # Zdefiniowanie wielkości etykiet
 plt.tick_params(axis="both", which="major", labelsize=14)
 # Zdefiniowanie zakresu dla każdej osi.
-plt.axis([0, 1100, 0, 1100000])
+
 plt.show()
