a8e7336bc2102096300f9b6655595b6c18d33a20
Kamil Kałuża <kamil321.94@gmail.com>
2022-03-31 13:15:31 +0200

    Refaktoryzacja random walk

--- WizualizacjaDanych/random_walk.py
+++ WizualizacjaDanych/random_walk.py
@@ -12,18 +12,19 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice(list(range(1, 5)))
+        step = direction * distance
+        return step
+
     def fill_walk(self):
         """Wygenerowanie wszystkich punktów dla błądzenia losowego"""
         # Wykonanie kroków, aż do chwili osiągnięcia oczekiwanej liczby punktów.
         while len(self.x_values) < self.num_points:
             # Ustalenie kierunku oraz odległości do pokonania w tym kierunku
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
             # Odrzucenie kroków, które prowadzą donikąd.
             if x_step == 0 and y_step == 0:
                 continue
--- WizualizacjaDanych/rw_visual.py
+++ WizualizacjaDanych/rw_visual.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk(50000)
+    rw = RandomWalk(50)
     rw.fill_walk()
     # Określenie wielkości okna
     plt.figure(figsize=(14, 6))
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    plt.plot(rw.x_values, rw.y_values, linewidth=0.5)
     # Oznaczenie punktu początku i końca RW
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors='none', s=100)
