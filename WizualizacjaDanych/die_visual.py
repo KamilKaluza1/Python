7ad3f9205da29ab7224440ac21e6e1984ad95774
Kamil Kałuża <kamil321.94@gmail.com>
2022-04-06 18:00:23 +0200

    rzut kością automatyzacja

--- WizualizacjaDanych/die_visual.py
+++ WizualizacjaDanych/die_visual.py
@@ -2,10 +2,11 @@ import pygal
 
 from die import Die
 
-die_1 = Die()
-die_2 = Die()
+die_1 = Die(10)
+die_2 = Die(10)
 results = []
-for roll_num in range(1000):
+throws = 10000
+for roll_num in range(throws):
     result = die_2.roll() + die_1.roll()
     results.append(result)
 
@@ -18,11 +19,11 @@ for value in range(2, max_result + 1):
 
 # Wizualizacja wyników
 hist = pygal.Bar()
-hist.force_uri_protocol = 'http'
+hist.force_uri_protocol = 'https'
 
-hist.title = "Wyniki rzucania dwiema kośćmi D6 tysiąc razy."
-hist.x_labels = [str(x) for x in range(2, 13)]
+hist.title = f"Wyniki rzucania kośćmi D{die_1.num_sides} + D{die_2.num_sides} {throws} razy."
+hist.x_labels = [str(x) for x in range(2, (die_1.num_sides + die_2.num_sides) + 1)]
 hist.x_title = "Wynik"
 hist.y_title = "Częstotliwość występowania wartości."
-hist.add('D6 + D6', frequencies)
+hist.add(f'D{die_1.num_sides} + D{die_2.num_sides}', frequencies)
 hist.render_to_file('die_visual.svg')
