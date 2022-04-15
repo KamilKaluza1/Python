8957c6ede7f30b502cbf43a6f972c5019ecd1c78
Kamil Kałuża <kamil321.94@gmail.com>
2022-04-15 12:45:00 +0200

    Pygal World Map

---
+++ WizualizacjaDanych/country_codes.py
@@ -0,0 +1,8 @@
+from pygal.maps.world import COUNTRIES
+
+
+def get_country_code(country_name):
+    """ Zwraca stosowany przez Pygal dwuznakowy kod państwa przypisany danemu państwu. """
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
