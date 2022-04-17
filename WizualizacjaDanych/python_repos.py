d055afb8865af909f0457ea6ed45f946a68fe9af
Kamil Kałuża <kamil321.94@gmail.com>
2022-04-17 17:45:24 +0200

    Przetwarzamnie danych z API / pygal.Config()

--- WizualizacjaDanych/python_repos.py
+++ WizualizacjaDanych/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightStyle as LS
 
 # Wykonanie wywołania API i zachowanie otrzymanej odpowiedzi.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -14,12 +16,26 @@ print("Liczba zwróconych repozytoriów:", len(repo_dicts))
 # Przeanalizowanie pierwszego repozytorium.
 repo_dict = repo_dicts[0]
 
-print(f'\nWybrane informacje o pierwszym repozytorium:')
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print(f'Nazwa: {repo_dict["name"]}\n'
-          f'Właściciel: {repo_dict["owner"]["login"]}\n'
-          f'Gwiazdki: {repo_dict["stargazers_count"]}\n'
-          f'Repozytorium: {repo_dict["html_url"]}\n'
-          f'Utworzone: {repo_dict["created_at"]}\n'
-          f'Uaktualnione: {repo_dict["updated_at"]}\n'
-          f'Opis: {repo_dict["description"]}\n')
+    names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+# Utworzenie wizualizacji
+my_style = LS(base_style=LCS)
+chart = pygal.Bar(my_config, style=my_style)
+chart.force_uri_protocol = "http"
+chart.title = 'Oznaczone największą liczbą gwiazdek projekty Pythona w serwisie github'
+chart.x_labels = names
+chart.add("", stars)
+chart.render_to_file('python_repos.svg')
