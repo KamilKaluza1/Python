0baf9544157ad0f8247b922e14f63d4035377750
Kamil Kałuża <kamil321.94@gmail.com>
2022-04-23 13:06:26 +0200

    create ToDoList, Item

--- Django/mysite/main/views.py
+++ Django/mysite/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(response):
+    return HttpResponse("<h1>tech with tim!</hi>")
+def v1(response):
+    return HttpResponse("<h1>You see view1!</hi>")
